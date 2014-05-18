48e8ec314c2d214f8eb9e2fb3d3a782cfb25dcbd
Jason Bruce <jbruce12000@gmail.com>
2014-05-18 13:51:52 -0400

    - add doc strings

--- monte-carlo.py
+++ monte-carlo.py
@@ -14,10 +14,12 @@ class ticket(object):
         self.guesses = []
 
     def random_guesses(self, size=100000, stddevs=3.29):
+        """make size random guesses from median out to stddevs"""
         self.guesses = stats.norm.rvs(loc=self.mediandays, scale=stddevs, size=size)
         return self.guesses
 
     def negatives_to_zero(self,list):
+        """convert negative numbers to zero for a given list"""
         # outliers are sometimes negative.  I am torn on whether to remove
         # them.  hubbard says leave them.
         for index, item in enumerate(list):
@@ -37,21 +39,25 @@ class project(object):
         self.read_project()
 
     def mindays(self):
+        """sum of ticket mindays"""
         total = 0
         for t in self.tickets:
             total = total + t.mindays
         return total
 
     def maxdays(self):
+        """sum of ticket maxdays"""
         total = 0
         for t in self.tickets:
             total = total + t.maxdays
         return total
 
     def add_ticket(self,t):
+        """add a ticket to the project"""
         self.tickets.append(t)
 
     def read_project(self):
+        """read a project from a json file"""
         with open(self.file) as json_file:
             d = json.load(json_file)
             json_file.close()
@@ -64,9 +70,11 @@ class project(object):
             self.add_ticket(t); 
 
     def num_tickets(self):
+        """number of tickets in this project"""
         return len(self.tickets)
 
     def get_totals(self,iterations=100000):
+        """make random guesses for each ticket range and total them"""
         self.totals = [0] * iterations
         self.unparallelized_totals = [0] * iterations
         for t in self.tickets:
@@ -79,6 +87,7 @@ class project(object):
 
 
     def n_percentile(self,percentile=85):
+        """return the number of days at N percent"""
         hist = self.histogram()
         total = 0
         for item in hist:
@@ -93,6 +102,7 @@ class project(object):
                 return range[index]
 
     def google_histogram(self):
+        """tab separated histogram of days to guesses"""
         print "OK printing histogram"
         print "Day\tGuesses where project completed on that day"
         hist = self.histogram()
@@ -101,10 +111,12 @@ class project(object):
             print "%d\t%d" % (range[index],hist[index])
 
     def histogram(self):
+        """histogram of days to guesses"""
         range = self.range_of_ints(self.totals)
         return stats.histogram2(self.totals,range)
 
     def range_of_ints(self,alist):
+        """return range of integers for a given unordered list"""
         return range(int(min(alist)),int(max(alist))+1)
 
     def __str__(self):
